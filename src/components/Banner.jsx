9526791172a985aeea6fea63580a4a9980481e66
Μιχαίας <mmcadoulos@gmail.com>
2024-03-16 22:04:48 -0500

    finished but banner doesn't have css applied?

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './styling.css'
+
+const Banner = () => {
+  return (
+    <header>
+      Orbit Report
+      <p>
+        Click on the buttons to view the satellites in a specified orbit type
+      </p>
+    </header>
+  );
+};
+
+export default Banner;

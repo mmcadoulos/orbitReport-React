a7b20834891f2e3237d068947959e490d9735f27
Μιχαίας <mmcadoulos@gmail.com>
2024-03-21 13:04:15 -0500

    fixed banner for css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import './styling.css'
+import "./styling.css";
 
 const Banner = () => {
   return (
-    <h1>
-      Orbit Report
+    <>
+      <h1>Orbit Report</h1>
       <p>
         Click on the buttons to view the satellites in a specified orbit type
       </p>
-    </h1>
+    </>
   );
 };
 
